9166a06836a129952301c5868cfe0dc3e1e48cf1
Bhupendra211 <bhupendrabisht488@gmail.com>
2025-03-09 20:16:02 +0530

    Add some changes to default routes

--- src/app.js
+++ src/app.js
@@ -15,8 +15,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 dbConnect();
 
-app.get('/v1/', (req, res) => {
-    res.send("Hello world");
+app.get('api/v1/', (req, res) => {
+    res.send("Hello world! Welcome to Quiz Application");
 });
 
 app.use('/api/v1/auth', authRoutes);
